728c97bc7bec1e46991dda1f944a3fa0138bff6b
nurzainpradana <pradananurzain@gmail.com>
2020-12-25 17:40:07 +0700

    create mainActivity

--- app/src/main/java/com/zainpradana/belajarkotlin/belajardi/MainActivity.kt
+++ app/src/main/java/com/zainpradana/belajarkotlin/belajardi/MainActivity.kt
@@ -2,10 +2,27 @@ package com.zainpradana.belajarkotlin.belajardi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.zainpradana.belajarkotlin.belajardi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        // Membuat Instance atau Object dari class LightKnight NightKnigth
+        val lightKnight = LightKnight()
+        val darkKnight = DarkKnight()
+
+        // Menambahkan listener pada button
+        binding.clickMe.setOnClickListener{
+            // Menampilkan Data pada TextView yang diambil dari fungsi setEquip dari kedua class tsb
+            val getReport = lightKnight.setEquip() + " dan " + darkKnight.setEquip()
+            binding.report.text = getReport
+        }
+
     }
 }
