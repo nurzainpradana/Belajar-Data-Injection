978430d5d8fc1212b464dba928db43284d0bbbed
nurzainpradana <pradananurzain@gmail.com>
2020-12-26 22:54:57 +0700

    create BattleModule dan implement @Module  & @Provides

---
+++ app/src/main/java/com/zainpradana/belajarkotlin/belajardi/BattleModule.kt
@@ -0,0 +1,14 @@
+package com.zainpradana.belajarkotlin.belajardi
+
+import dagger.Module
+import dagger.Provides
+
+@Module
+class BattleModule {
+
+    // Menyediakan Dependency yang akan digunakan dengan parameter didalam Class Senjata
+    @Provides
+    fun setSenjata(): Senjata {
+        return Senjata("Pedang larva", "Tombak halilintar")
+    }
+}
--- app/src/main/java/com/zainpradana/belajarkotlin/belajardi/Senjata.kt
+++ app/src/main/java/com/zainpradana/belajarkotlin/belajardi/Senjata.kt
@@ -1,7 +1,9 @@
 package com.zainpradana.belajarkotlin.belajardi
 
 //Class ini nantinya akan diakses oleh Class LightKnight dan DarkKnight
-class Senjata (private val namaPedang: String, private val namaTombak: String) {
+class Senjata (private val namaPedang: String,
+               private val namaTombak: String) {
+
     //Membuat 2 Buah Fungsi yang Mengenbalikan Nilai String dengan Data Berbeda
     fun tombak(): String {
 //        return "Tombak"
